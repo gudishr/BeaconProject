283d38d060622e0c03d591c80dec3603d465e992
shruti gudi <shrutigudi@gmail.com>
2019-12-04 16:59:13 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,116 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { DeviceEventEmitter } from 'react-native'
+import Beacons from 'react-native-beacons-manager'
+
+export default class MyComponent extends Component {
+  componentDidMount(){ 
+    // Define a region which can be identifier + uuid,
+    // identifier + uuid + major or identifier + uuid + major + minor
+    // (minor and major properties are numbers)
+    const region = {
+        identifier: 'Estimotes',
+        uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
+    };
+    
+    // Request for authorization while the app is open
+    Beacons.requestWhenInUseAuthorization();
+    
+    Beacons.startMonitoringForRegion(region);
+    Beacons.startRangingBeaconsInRegion(region);
+    
+    Beacons.startUpdatingLocation();
+    
+    // Listen for beacon changes
+    const subscription = DeviceEventEmitter.addListener(
+      'beaconsDidRange',
+      (data) => {
+        // data.region - The current region
+        // data.region.identifier
+        // data.region.uuid
+    
+        // data.beacons - Array of all beacons inside a region
+        //  in the following structure:
+        //    .uuid
+        //    .major - The major version of a beacon
+        //    .minor - The minor version of a beacon
+        //    .rssi - Signal strength: RSSI value (between -100 and 0)
+        //    .proximity - Proximity value, can either be "unknown", "far", "near" or "immediate"
+        //    .accuracy - The accuracy of a beacon
+      }
+    );
+  }
+
+
+  render(){
+    return(
+      <View style={styles.body}>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Welcome</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
